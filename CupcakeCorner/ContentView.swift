d1309e9f8d07190e79309ad47cecfa34882d0ad9
Alex Bonder <lexbonder@gmail.com>
2023-08-06 11:51:36 -0400

    Validating and disabling forms

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -8,35 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username = ""
+    @State private var email = ""
     
     var body: some View {
-// add scale: for swift to create the 1x, 2x, and 3x versions.
-// This image was built to be 1200 pixels. If I label it as 3x, swift knows that 1x is 400 pixels, and displays that.
-        // AsyncImage(url: URL(string: "https://hws.dev/img/logo.png"), scale: 3)
-        
-// Add closure to specify conditions for the returned image. then image responds to frame modifier
-        // AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { image in
-        //     image
-        //         .resizable()
-        //         .scaledToFit()
-        //     } placeholder: {
-        //         ProgressView()
-        //     }
-        //     .frame(width: 200, height: 200) // frame works only on the pre-loaded view
-        
-// "phase in" is different from "image in" - phase gives better error handling
-        AsyncImage(url: URL(string: "https://hws.dev/img/bad.png")) { phase in
-            if let image = phase.image { // successful download
-                image
-                    .resizable()
-                    .scaledToFit()
-            } else if phase.error != nil { // failed download
-                Text("There was an error loading the image.")
-            } else { // until success or fail
-                ProgressView()
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
             }
+            
+            Section {
+                Button("Create Account") {
+                    print("Creating Account...")
+                }
+            }
+            .disabled(disableForm) // recieves a boolean
         }
-        .frame(width: 200, height: 200)
+    }
+    
+    var disableForm: Bool {
+        username.count < 5 || email.count < 5
     }
 }
 
